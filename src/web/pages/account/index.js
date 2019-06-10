58713153a8c243cd0f488d09984740cc290ac903
sarpisik <contact@sarpisik.com>
2019-06-10 18:35:50 +0200

    Edit syntax fix

--- src/web/pages/account/index.js
+++ src/web/pages/account/index.js
@@ -4,19 +4,15 @@ import { withAuthorization } from '../../session';
 import * as ROUTES from '../../constants/routes';
 import { PageTitle, PageBody, SignOut } from '../../components';
 
-const AccountPage = ({ authUser, signOut }) => {
-  console.log(authUser);
-
-  return (
-    <Container>
-      <Row className="justify-content-center bg-light">
-        <PageTitle title="My Account" />
-        <PageBody sm={4}>
-          <SignOut onClick={signOut} />
-        </PageBody>
-      </Row>
-    </Container>
-  );
-};
+const AccountPage = ({ signOut }) => (
+  <Container>
+    <Row className="justify-content-center bg-light">
+      <PageTitle title="My Account" />
+      <PageBody sm={4}>
+        <SignOut onClick={signOut} />
+      </PageBody>
+    </Row>
+  </Container>
+);
 
 export default withAuthorization(true)(ROUTES.LOGIN)(AccountPage);
