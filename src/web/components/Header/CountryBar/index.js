f0c7b3731f2368da892483668655bf765e16fc5f
sarpisik <contact@sarpisik.com>
2019-06-10 18:34:58 +0200

    Edit syntax fix

--- src/web/components/Header/CountryBar/index.js
+++ src/web/components/Header/CountryBar/index.js
@@ -3,16 +3,16 @@ import { Col, Card } from 'react-bootstrap';
 import Icon from '../../Icon';
 import { withGeoLocation } from '../../../session';
 
-const CountryBar = ({ location }) =>
-  location.country ? (
+const CountryBar = ({ location: { country = null } }) =>
+  country && (
     <Col sm={12} className="text-left">
       <Card bg="dark" className="country border-0 rounded-0" text="white">
         <Card.Body className="px-0">
           <Icon icon="dolly" /> Deliver to:{' '}
-          <span className="text-info">{location.country}</span>
+          <span className="text-info">{country}</span>
         </Card.Body>
       </Card>
     </Col>
-  ) : null;
+  );
 
 export default withGeoLocation(CountryBar);
