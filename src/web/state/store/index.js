387b84e8e2bd895e06f9b6ccf200958c69ce1ced
sarpisik <contact@sarpisik.com>
2019-06-10 13:03:08 +0200

    Disable redux logger

--- src/web/state/store/index.js
+++ src/web/state/store/index.js
@@ -1,16 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+// import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const logger = createLogger()
+// const logger = createLogger()
 
 const store = createStore(
   rootReducer,
   undefined,
-  storeEnhancers(applyMiddleware(logger, thunk))
-)
+  storeEnhancers(
+    applyMiddleware(
+      // logger,
+      thunk
+    )
+  )
+);
 
-export default store
+export default store;
